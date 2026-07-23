c2d0fe0038ebb607a6a83351980ce0b62f178cd4
Huei Feng <695979933@qq.com>
2026-07-23 23:12:04 +0800

    refactor(postgres): remove legacy repository and lock provider

--- src/KubeJob.Storage.PostgreSQL/Extensions/KubeJobPostgresExtensions.cs
+++ src/KubeJob.Storage.PostgreSQL/Extensions/KubeJobPostgresExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using KubeJob.Server.Data;
 using KubeJob.Server.Options;
 using KubeJob.Server.Runtime;
@@ -7,43 +6,37 @@ using KubeJob.Storage.PostgreSQL.Runtime;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 
-namespace KubeJob.Storage.PostgreSQL.Extensions
+namespace KubeJob.Storage.PostgreSQL.Extensions;
+
+public static class KubeJobPostgresExtensions
 {
-    public static class KubeJobPostgresExtensions
+    public static KubeJobServerOptions UsePostgreSql(
+        this KubeJobServerOptions options,
+        string connectionString)
     {
-        public static KubeJobServerOptions UsePostgreSql(this KubeJobServerOptions options, string connectionString = "")
+        ArgumentNullException.ThrowIfNull(options);
+        if (string.IsNullOrWhiteSpace(connectionString))
         {
-            ArgumentNullException.ThrowIfNull(options);
-            if (string.IsNullOrWhiteSpace(connectionString))
-            {
-                throw new ArgumentException("A PostgreSQL connection string is required.", nameof(connectionString));
-            }
-
-            options.StorageConfigurator = services =>
-            {
-                services.AddSingleton<IKubeJobRepository>(_ => new KubeJobRepository(connectionString));
-                services.AddSingleton<KubeJob.Server.Data.IStorageInitializer>(_ => new DbInitializer(connectionString));
-
-                services.AddSingleton(_ => NpgsqlDataSource.Create(connectionString));
-                services.AddSingleton<PostgreSqlJobRuntimeStore>();
-                services.AddSingleton<IJobSubmissionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IWorkerSessionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IJobClaimStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IJobCompletionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IJobQueryStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IJobScheduleStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-                services.AddSingleton<IOutboxStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
-            };
-
-            if (options.LockConfigurator == null)
-            {
-                options.LockConfigurator = services =>
-                {
-                    services.AddSingleton<IKubeJobLockProvider>(_ => new PostgreSqlLockProvider(connectionString));
-                };
-            }
-
-            return options;
+            throw new ArgumentException(
+                "A PostgreSQL connection string is required.",
+                nameof(connectionString));
         }
+
+        options.StorageConfigurator = services =>
+        {
+            services.AddSingleton<IStorageInitializer>(_ => new DbInitializer(connectionString));
+            services.AddSingleton(_ => NpgsqlDataSource.Create(connectionString));
+            services.AddSingleton<PostgreSqlJobRuntimeStore>();
+            services.AddSingleton<IJobSubmissionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IWorkerSessionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IJobClaimStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IJobCompletionStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IJobQueryStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IJobScheduleStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IOutboxStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+            services.AddSingleton<IJobRuntimeDashboardStore>(sp => sp.GetRequiredService<PostgreSqlJobRuntimeStore>());
+        };
+
+        return options;
     }
 }
