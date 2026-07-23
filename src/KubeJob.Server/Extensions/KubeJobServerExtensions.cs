b8a4abf917b3ff15eab02c32b0637f7a01166090
Huei Feng <695979933@qq.com>
2026-07-23 23:09:41 +0800

    refactor(server): remove legacy control-plane registration

--- src/KubeJob.Server/Extensions/KubeJobServerExtensions.cs
+++ src/KubeJob.Server/Extensions/KubeJobServerExtensions.cs
@@ -1,122 +1,89 @@
-using System;
 using KubeJob.Core.Client;
 using KubeJob.Core.Scheduling;
-using KubeJob.Server.Data;
 using KubeJob.Server.Options;
 using KubeJob.Server.Runtime;
-using KubeJob.Server.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace KubeJob.Server.Extensions
+namespace KubeJob.Server.Extensions;
+
+public static class KubeJobServerExtensions
 {
-    public static class KubeJobServerExtensions
+    public static IServiceCollection AddKubeJobServer(
+        this IServiceCollection services,
+        Action<KubeJobServerOptions>? configure = null)
     {
-        public static IServiceCollection AddKubeJobServer(this IServiceCollection services, Action<KubeJobServerOptions>? configure = null)
-        {
-            var options = new KubeJobServerOptions();
-            configure?.Invoke(options);
+        var options = new KubeJobServerOptions();
+        configure?.Invoke(options);
+        options.StorageConfigurator?.Invoke(services);
 
-            services.AddSingleton<IServerIdentity, DefaultServerIdentity>();
+        services.TryAddSingleton<InMemoryJobRuntimeStore>();
+        services.TryAddSingleton<IJobSubmissionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IWorkerSessionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IJobClaimStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IJobCompletionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IJobQueryStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IJobScheduleStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IOutboxStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IJobRuntimeDashboardStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
+        services.TryAddSingleton<IWorkAvailableNotifier, PollingWorkAvailableNotifier>();
+        services.TryAddSingleton<IJobClient, DefaultJobClient>();
+        services.TryAddSingleton<IJobScheduleClient, DefaultJobScheduleClient>();
+        services.AddOptions<JobRuntimeOptions>();
 
-            if (options.StorageConfigurator != null)
-            {
-                options.StorageConfigurator(services);
-            }
-            else
-            {
-                services.AddSingleton<IKubeJobRepository, InMemoryKubeJobRepository>();
-            }
-
-            // Runtime V2 is additive. Durable providers register these interfaces in
-            // StorageConfigurator; otherwise the reference in-memory state machine is used.
-            services.TryAddSingleton<InMemoryJobRuntimeStore>();
-            services.TryAddSingleton<IJobSubmissionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IWorkerSessionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IJobClaimStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IJobCompletionStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IJobQueryStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IJobScheduleStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IOutboxStore>(sp => sp.GetRequiredService<InMemoryJobRuntimeStore>());
-            services.TryAddSingleton<IWorkAvailableNotifier, PollingWorkAvailableNotifier>();
-            services.TryAddSingleton<IJobClient, DefaultJobClient>();
-            services.TryAddSingleton<IJobScheduleClient, DefaultJobScheduleClient>();
-            services.AddOptions<JobRuntimeOptions>();
-
-            if (options.LockConfigurator != null)
-            {
-                options.LockConfigurator(services);
-            }
-            else
-            {
-                services.AddSingleton<IKubeJobLockProvider, InMemoryLockProvider>();
-            }
-
-            services.AddControllers();
-
-            if (options.EnableLegacyHostedServices)
-            {
-                services.AddHostedService<CronSchedulerService>();
-                services.AddHostedService<JobDispatcherService>();
-                services.AddHostedService<NodeHealthService>();
-                services.AddHostedService<HistoryCleanupService>();
-            }
-
-            if (options.EnableV2HostedServices)
-            {
-                services.AddHostedService<ScheduleReconcilerService>();
-                services.AddHostedService<LeaseReaperService>();
-                services.AddHostedService<OutboxPublisherService>();
-            }
-
-            return services;
-        }
-
-        public static IServiceCollection AddKubeJobDashboard(this IServiceCollection services, string routePrefix = "kubejob")
-        {
-            services.AddControllersWithViews(options =>
-            {
-                options.Conventions.Add(new KubeJobDashboardRouteConvention(routePrefix));
-            });
-            return services;
-        }
-
-        public static void InitializeKubeJobDatabase(this IApplicationBuilder app)
-        {
-            using var scope = app.ApplicationServices.CreateScope();
-            var init = scope.ServiceProvider.GetService<KubeJob.Server.Data.IStorageInitializer>();
-            init?.Initialize();
-        }
+        services.AddControllers();
+        services.AddHostedService<ScheduleReconcilerService>();
+        services.AddHostedService<LeaseReaperService>();
+        services.AddHostedService<OutboxPublisherService>();
+        return services;
     }
 
-    public class KubeJobDashboardRouteConvention : IControllerModelConvention
+    public static IServiceCollection AddKubeJobDashboard(
+        this IServiceCollection services,
+        string routePrefix = "kubejob")
     {
-        private readonly string _routePrefix;
-        public KubeJobDashboardRouteConvention(string routePrefix)
+        services.AddControllersWithViews(options =>
         {
-            _routePrefix = routePrefix.Trim('/');
+            options.Conventions.Add(new KubeJobDashboardRouteConvention(routePrefix));
+        });
+        return services;
+    }
+
+    public static void InitializeKubeJobDatabase(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var initializer = scope.ServiceProvider.GetService<KubeJob.Server.Data.IStorageInitializer>();
+        initializer?.Initialize();
+    }
+}
+
+public sealed class KubeJobDashboardRouteConvention : IControllerModelConvention
+{
+    private readonly string _routePrefix;
+
+    public KubeJobDashboardRouteConvention(string routePrefix)
+    {
+        _routePrefix = string.IsNullOrWhiteSpace(routePrefix)
+            ? "kubejob"
+            : routePrefix.Trim('/');
+    }
+
+    public void Apply(ControllerModel controller)
+    {
+        if (controller.ControllerType.Name != "DashboardController")
+        {
+            return;
         }
 
-        public void Apply(ControllerModel controller)
+        foreach (var selector in controller.Selectors)
         {
-            if (controller.ControllerType.Name == "DashboardController")
-            {
-                foreach (var selector in controller.Selectors)
-                {
-                    if (selector.AttributeRouteModel != null)
-                    {
-                        selector.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(
-                            new AttributeRouteModel { Template = _routePrefix },
-                            selector.AttributeRouteModel);
-                    }
-                    else
-                    {
-                        selector.AttributeRouteModel = new AttributeRouteModel { Template = _routePrefix };
-                    }
-                }
-            }
+            selector.AttributeRouteModel = selector.AttributeRouteModel is null
+                ? new AttributeRouteModel { Template = _routePrefix }
+                : AttributeRouteModel.CombineAttributeRouteModel(
+                    new AttributeRouteModel { Template = _routePrefix },
+                    selector.AttributeRouteModel);
         }
     }
 }
