830125c39191cc3254cb32572ed972b4c59fc8d5
Huei Feng <695979933@qq.com>
2026-07-23 23:41:23 +0800

    test: validate V2-only hosting defaults

--- tests/KubeJob.Tests/Runtime/HostingConfigurationTests.cs
+++ tests/KubeJob.Tests/Runtime/HostingConfigurationTests.cs
@@ -1,7 +1,6 @@
 using FluentAssertions;
 using KubeJob.Server.Extensions;
 using KubeJob.Server.Runtime;
-using KubeJob.Server.Services;
 using KubeJob.Storage.PostgreSQL.Extensions;
 using KubeJob.Storage.PostgreSQL.Runtime;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,49 +11,31 @@ namespace KubeJob.Tests.Runtime;
 public sealed class HostingConfigurationTests
 {
     [Fact]
-    public void V2_only_registers_only_V2_background_services()
+    public void Server_registers_the_complete_V2_control_plane()
     {
         var services = new ServiceCollection();
 
-        services.AddKubeJobServer(options => options.UseV2Only());
+        services.AddKubeJobServer();
 
         HasHostedService<ScheduleReconcilerService>(services).Should().BeTrue();
         HasHostedService<LeaseReaperService>(services).Should().BeTrue();
         HasHostedService<OutboxPublisherService>(services).Should().BeTrue();
-        HasHostedService<CronSchedulerService>(services).Should().BeFalse();
-        HasHostedService<JobDispatcherService>(services).Should().BeFalse();
-        HasHostedService<NodeHealthService>(services).Should().BeFalse();
-        HasHostedService<HistoryCleanupService>(services).Should().BeFalse();
     }
 
     [Fact]
-    public void Legacy_only_registers_only_legacy_background_services()
-    {
-        var services = new ServiceCollection();
-
-        services.AddKubeJobServer(options => options.UseLegacyOnly());
-
-        HasHostedService<CronSchedulerService>(services).Should().BeTrue();
-        HasHostedService<JobDispatcherService>(services).Should().BeTrue();
-        HasHostedService<NodeHealthService>(services).Should().BeTrue();
-        HasHostedService<HistoryCleanupService>(services).Should().BeTrue();
-        HasHostedService<ScheduleReconcilerService>(services).Should().BeFalse();
-        HasHostedService<LeaseReaperService>(services).Should().BeFalse();
-        HasHostedService<OutboxPublisherService>(services).Should().BeFalse();
-    }
-
-    [Fact]
-    public void PostgreSQL_registration_uses_the_durable_store_for_schedules()
+    public void PostgreSQL_registration_uses_one_durable_store_for_runtime_and_dashboard()
     {
         var services = new ServiceCollection();
         services.AddLogging();
         services.AddKubeJobServer(options =>
-            options.UseV2Only().UsePostgreSql(
+            options.UsePostgreSql(
                 "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=postgres"));
 
         using var provider = services.BuildServiceProvider();
 
         provider.GetRequiredService<IJobScheduleStore>()
+            .Should().BeOfType<PostgreSqlJobRuntimeStore>();
+        provider.GetRequiredService<IJobRuntimeDashboardStore>()
             .Should().BeOfType<PostgreSqlJobRuntimeStore>();
     }
 
