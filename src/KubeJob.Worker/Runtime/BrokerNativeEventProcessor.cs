2e3475213e2dd6ad2f42a05b740e6e9981df411c
Huei Feng <695979933@qq.com>
2026-08-08 13:56:58 +0800

    validate event workers without requiring job queues

--- src/KubeJob.Worker/Runtime/BrokerNativeEventProcessor.cs
+++ src/KubeJob.Worker/Runtime/BrokerNativeEventProcessor.cs
@@ -27,7 +27,7 @@ public sealed class BrokerNativeEventProcessor
     {
         _executionEngine = executionEngine;
         _worker = worker.Value;
-        _worker.Validate();
+        _worker.Validate(requireJobQueues: false);
     }
 
     public async ValueTask<BrokerNativeEventProcessingResult> ProcessAsync(
